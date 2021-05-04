5a5c01b09ea8fc35bd85a82c9a11fa4d862baad5
oZeroLuck <50614854+oZeroLuck@users.noreply.github.com>
2021-05-04 17:50:46 +0200

    Test DTO

---
+++ src/main/java/com/testservice/webapp/dto/UserDto.java
@@ -0,0 +1,72 @@
+package com.testservice.webapp.dto;
+
+import com.testservice.webapp.entity.User;
+
+import java.util.Date;
+
+public class UserDto {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private Date birthDate;
+
+    public UserDto(User user) {
+        this.id = user.getId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.username = user.getUsername();
+        this.birthDate = user.getBirthDate();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String name) {
+        this.firstName = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
